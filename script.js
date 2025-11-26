fc9f6c91fdca2794f86fb7edf4eecc3489b76fd1
Aisyrga <itsmeais15@gamail.com>
2025-11-27 05:02:44 +0600

    init commit

---
+++ script.js
@@ -0,0 +1,22 @@
+let color = prompt("Введите цвет светофора на англ (red, yellow, green)");
+
+let red = document.getElementById("red");
+let yellow = document.getElementById("yellow");
+let green = document.getElementById("green");
+
+if (color === "red") {
+    red.style.background = "red";
+    red.innerText = "STOP";
+}
+else if (color === "yellow") {
+    yellow.style.background = "yellow";
+    yellow.style.color = "black"
+    yellow.innerText = "WAIT";
+}
+else if (color === "green") {
+    green.style.background = "green";
+    green.innerText = "GO";
+}
+else {
+    alert("не тупите! у светофора только 3 цвета")
+}
